1e2929b85e51b39a3eb480f5e3e756f2ee9e2784
Joe <trends_boyish.0l@icloud.com>
2023-12-03 18:05:49 +1300

    feats: add restaurant screens

--- rn_example/screens/FoodApp/RestaurantScreen.js
+++ rn_example/screens/FoodApp/RestaurantScreen.js
@@ -1,13 +1,49 @@
 /**
  * Created by zuoxiansheng on 2/12/23
  */
-import { React } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { React, useLayoutEffect } from 'react'
+import {
+  Image,
+  ScrollView,
+  StyleSheet, Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { useRoute } from '@react-navigation/native'
+import { ArrowLeft } from 'react-native-feather'
+import { themeColors } from '../../theme'
 
-export default function RestaurantScreen() {
+export default function RestaurantScreen({navigation}) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false, // 这会隐藏导航栏
+    })
+  }, [navigation])
+
+  const {params} = useRoute()
+  console.log(params)
   return (
     <View>
-      <Text>Resturant</Text>
+      <ScrollView>
+        <View className="relative">
+          <Image source={params.image} className="w-full h-72"/>
+          <TouchableOpacity
+            className="absolute top-8 left-4 bg-white p-2 rounded-full"
+            onPress={() => {
+              navigation.goBack()
+            }}>
+            <ArrowLeft
+              strokeWidth={3}
+              stroke={themeColors.bgColor(1)}/>
+          </TouchableOpacity>
+        </View>
+        <View style={{borderTopLeftRadius: 40, borderTopRightRadius: 40}}
+              className="bg-white -mt-12 pt-6">
+          <View className="px-3">
+            <Text className="text-3xl font-bold">{params.name}</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
